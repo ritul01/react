9fcdeb53635cb88e1b9059792d66b785008a477e
Singh <ritulsingh123700@gmail.com>
2024-04-07 23:42:47 +0530

    after appwrite

---
+++ blogapp/src/conf/conf.js
@@ -0,0 +1,8 @@
+const conf={
+    appwriteURL:String(process.env.REACT_APP_APPWRITE_URL),
+    appwriteProjectID:String(process.env.REACT_APP_APPWRITE_PROJECT_ID),
+    appwriteDatabaseID:String(process.env.REACT_APP_APPWRITE_DATABASE_ID),
+    appwriteCollectionID:String(process.env.REACT_APP_APPWRITE_COLLECTION_ID),
+    appwriteBucketID:String(process.env.REACT_APP_APPWRITE_BUCKET_ID),
+}
+export default conf
